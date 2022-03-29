e8da3da06456ad5325b0a63b4c4030216d00ff77
dododamba <dominiquedamba@outlook.com>
2022-03-29 23:32:26 +0200

    update request 2

--- src/main/java/com/plm/controller/PartController.java
+++ src/main/java/com/plm/controller/PartController.java
@@ -4,7 +4,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,8 +33,8 @@ public class PartController {
 	private PartDAO partDAO;
 
 	@Autowired
-	public PartController() {
-
+	public PartController(PartDAO partDAO) {
+      this.partDAO = partDAO;
 	}
 
 	@PostMapping(value = "/part/reserve")
